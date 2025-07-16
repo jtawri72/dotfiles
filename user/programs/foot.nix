5f778a72304aa1a81922764e6391c6b5213542c6
jtawri72 <jtawri72@gmail.com>
2025-07-16 17:15:15 +0530

    dotfiles: user: foot: set font

--- user/programs/foot.nix
+++ user/programs/foot.nix
@@ -2,6 +2,17 @@
   config,
   pkgs,
   ...
-}: {
-  programs.foot.enable = true;
+}: let
+  font = "JetBrainsMono Nerd Font";
+  size = 11;
+in {
+  programs.foot = {
+    enable = true;
+    settings.main = {
+      font = "${font}:size=${builtins.toString size}:style=SemiBold";
+      font-bold = "${font}:size=${builtins.toString size}:style=Bold";
+      font-italic = "${font}:size=${builtins.toString size}:style=SemiBold Italic";
+      font-bold-italic = "${font}:size=${builtins.toString size}:style=Bold Italic";
+    };
+  };
 }
