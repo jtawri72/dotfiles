4acc9555922dc99726d94d39df350b878bf739a4
jtawri72 <jtawri72@gmail.com>
2025-07-16 17:36:35 +0530

    dotfiles: user: starship: set git branch symbol

--- user/programs/starship.nix
+++ user/programs/starship.nix
@@ -5,13 +5,17 @@
 }: {
   programs.starship = {
     enable = true;
-    settings.character = {
-      success_symbol = "[](bold green)";
-      error_symbol = "[](bold red)";
-      vimcmd_symbol = "[](bold green)";
-      vimcmd_replace_one_symbol = "[](bold red)";
-      vimcmd_replace_symbol = "[](bold red)";
-      vimcmd_visual_symbol = "[](bold yellow)";
+    settings = {
+      character = {
+        success_symbol = "[](bold green)";
+        error_symbol = "[](bold red)";
+        vimcmd_symbol = "[](bold green)";
+        vimcmd_replace_one_symbol = "[](bold red)";
+        vimcmd_replace_symbol = "[](bold red)";
+        vimcmd_visual_symbol = "[](bold yellow)";
+      };
+
+      git_branch.symbol = " ";
     };
   };
 }
